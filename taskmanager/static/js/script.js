287875b0a29acab55aa947fc1003816bbbc5c8c0
Dustin Li <dustin.li1988@gmail.com>
2022-06-11 12:29:37 +0000

    Link Jinja base.html templates to home.html

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -11,3 +11,8 @@ $(document).ready(function(){
             done: "Select"
         }
     });
+
+    var elem = document.querySelector('.collapsible.expandable');
+    var instance = M.Collapsible.init(elem, {
+    accordion: false
+    });
