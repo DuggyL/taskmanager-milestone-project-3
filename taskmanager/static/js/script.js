d775441f31a551f11e38e04f06d2d3544f6803e5
Dustin Li <dustin.li1988@gmail.com>
2022-06-11 12:12:00 +0000

    Setting up materializeframe work

---
+++ taskmanager/static/js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.sidenav').sidenav({edge: "right"});
+    $('.collapsible').collapsible();
+    $(".tooltipped").tooltip();
+    $('select').formSelect();
+    $('.datepicker').datepicker({
+        format: "dd mmmm yyyy",
+        yearRange: 1,
+        showClearBtn: true,
+        i18n: {
+            done: "Select"
+        }
+    });
