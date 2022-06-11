5607810a310adb32aac80352fef110e60e1c3f01
Dustin Li <dustin.li1988@gmail.com>
2022-06-11 14:13:02 +0000

    Add flash warning to to base.html

--- taskmanager/auth.py
+++ taskmanager/auth.py
@@ -1,13 +1,30 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
+    data = request.form
+    print(data)
     return render_template("login.html")
 
-@auth.route('/register')
+@auth.route('/register', methods=['GET', 'POST'])
 def register():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(username) < 3:
+            flash('username must be greater than 3 characters.', category='error')
+        elif len(password1) < 6:
+            flash('password must be at least 6 characters.', category='error')
+        elif password1 != password2:
+            flash('Password do not match.', category='error')
+        else:
+            # Add user to database
+            flash('You are registered.', category='success')
+        
     return render_template("register.html")
 
 @auth.route('/logout')
