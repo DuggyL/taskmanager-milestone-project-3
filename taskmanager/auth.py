bfeaf78f33c1c1b5cd1fef8e820d3ba071882c33
Dustin Li <dustin.li1988@gmail.com>
2022-06-11 12:47:08 +0000

    Setting up login form in login page

--- taskmanager/auth.py
+++ taskmanager/auth.py
@@ -1,15 +1,15 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login')
 def login():
-    return "<p>Login</p>"
+    return render_template("login.html")
+
+@auth.route('/register')
+def register():
+    return render_template("register.html")
 
 @auth.route('/logout')
 def logoit():
     return "<p>Logout</p>"
-
-@auth.route('/register')
-def register():
-    return "<p>Register</p>"
